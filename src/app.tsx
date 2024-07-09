f96bb58beb0a144a554370b8937700c572a5c02e
Valter B Freitas Jr <valter191@hotmail.com>
2024-07-09 20:36:45 -0300

    feat: added home page

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,137 @@
-export function App() {
-  return (
-    <div className="max-w-screen-md flex flex-1 justify-items-center">
-      <p>Convide seus amigos e planeje sua próxima viagem!</p>
+import {
+  MapPin,
+  Calendar,
+  ArrowRight,
+  UserRoundPlus,
+  Settings2,
+  X,
+} from "lucide-react";
+import { useState } from "react";
 
-      <p>
-        Ao planejar sua viagem pela plann.er você automaticamente concorda
-        com nossos <a href="/">termos de uso</a> e <a href="/">políticas de segurança</a>.
-      </p>
+export function App() {
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false);
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
+
+  const openGuestsInput = () => {
+    setIsGuestsInputOpen(true);
+  };
+
+  const closeGuestesInput = () => {
+    setIsGuestsInputOpen(false);
+  };
+
+  const openGuestsModal = () => {
+    setIsGuestsModalOpen(true);
+  };
+
+  const closeGuestsModal = () => {
+    setIsGuestsModalOpen(false);
+  };
+
+  return (
+    <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo.svg" alt="logo plann.er" />
+          <p className="text-zinc-300 text-lg">
+            Convide seus amigos e planeje sua próxima viagem!
+          </p>
+        </div>
+
+        <div className="space-y-5">
+          <div className="h-16 px-4 bg-zinc-900 rounded-xl flex items-center shadow-shape gap-3">
+            <div className="flex gap-2 items-center flex-1">
+              <MapPin className="size-5 text-zinc-400" />
+              <input
+                type="text"
+                placeholder="Para onde você vai?"
+                className="bg-transparent text-lg placeholder:text-zinc-400 outline-none w-full"
+                disabled={isGuestsInputOpen}
+              />
+            </div>
+
+            <div className="flex gap-2 items-center">
+              <Calendar className="size-5 text-zinc-400" />
+              <input
+                type="text"
+                placeholder="Quando?"
+                className="bg-transparent text-lg placeholder:text-zinc-400 outline-none w-40"
+                disabled={isGuestsInputOpen}
+              />
+            </div>
+
+            <div className="w-px h-6 bg-zinc-800" />
+
+            {isGuestsInputOpen ? (
+              <button
+                className="text-zinc-200 bg-zinc-800 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700"
+                onClick={closeGuestesInput}
+              >
+                Alterar local/data
+                <Settings2 className="size-5" />
+              </button>
+            ) : (
+              <button
+                className="text-lime-950 bg-lime-300 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+                onClick={openGuestsInput}
+              >
+                Continuar
+                <ArrowRight className="size-5" />
+              </button>
+            )}
+          </div>
+
+          {isGuestsInputOpen && (
+            <div className="h-16 w-full px-4 bg-zinc-900 rounded-xl flex items-center shadow-shape gap-3">
+              <button
+                className="flex gap-2 items-center flex-1"
+                onClick={openGuestsModal}
+              >
+                <UserRoundPlus className="size-5 text-zinc-400" />
+                <span className="bg-transparent text-lg text-zinc-400 outline-none w-full text-left">
+                  Quem estará na viagem?
+                </span>
+              </button>
+
+              <div className="w-px h-6 bg-zinc-800" />
+
+              <button
+                className="text-lime-950 bg-lime-300 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+                onClick={openGuestsInput}
+              >
+                Confirmar viagem
+                <ArrowRight className="size-5" />
+              </button>
+            </div>
+          )}
+        </div>
+
+        <p className="text-zinc-500 text-sm text-center">
+          Ao planejar sua viagem pela plann.er você automaticamente concorda{" "}
+          <br /> com nossos{" "}
+          <a className="text-zinc-300 underline" href="/">
+            termos de uso
+          </a>{" "}
+          e{" "}
+          <a className="text-zinc-300 underline" href="/">
+            políticas de privacidade
+          </a>
+          .
+        </p>
+      </div>
+
+      {isGuestsModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900">
+            <div className="flex justify-between items-center">
+              <h2 className="text-lg">Selecionar convidados</h2>
+              <button onClick={closeGuestsModal}>
+                <X className="size-5 text-zinc-400" />
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
