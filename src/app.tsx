b1a2348dbeb22c123901b39401b943274412b902
Valter B Freitas Jr <valter191@hotmail.com>
2024-07-09 00:23:43 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <div className="max-w-screen-md flex flex-1 justify-items-center">
+      <p>Convide seus amigos e planeje sua próxima viagem!</p>
+
+      <p>
+        Ao planejar sua viagem pela plann.er você automaticamente concorda
+        com nossos <a href="/">termos de uso</a> e <a href="/">políticas de segurança</a>.
+      </p>
+    </div>
+  )
+}
