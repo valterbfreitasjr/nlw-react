35232f6072b0718bbd340d0b73007fc89a0f8de5
Valter B Freitas Jr <valter191@hotmail.com>
2024-07-25 00:25:06 -0300

    feat: trip details imagem

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,18 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import TripDetailsPage from "./pages/trip-details";
 import { CreateTripPage } from "./pages/create-trip";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <CreateTripPage/>,
+    element: <CreateTripPage />,
   },
   {
-    path: '/trips/123',
-    element: <TripDetailsPage/>
-  }
+    path: "/trips/123",
+    element: <TripDetailsPage />,
+  },
 ]);
 
 export function App() {
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,51 +1,70 @@
-import { Calendar, MapPin, Plus, Settings2 } from 'lucide-react';
+import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
 
 const TripDetailsPage = () => {
-    return (
-        <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
-            <div className="flex items-center justify-between h-16 px-4 rounded-xl bg-zinc-900 shadow-shape">
-                <div className='flex items-center gap-2'>
-                    <MapPin className='size-5 text-zinc-400'/>
-                    <span className='text-zinc-100'>Florianópolis, Brasil</span>
-                </div>
-
-                <div className='flex items-center gap-5'>
-                    <div className='flex items-center gap-2'>
-                        <Calendar className='size-5 text-zinc-400'/>
-                        <span className='text-zinc-100'>17 a 23 de Agosto</span>
-                    </div>
-
-                    <div className='w-px h-6 bg-zinc-800'/>
-
-                    <button
-                        className="flex items-center gap-2 px-5 py-2 font-medium rounded-lg text-zinc-200 bg-zinc-800 hover:bg-zinc-700"
-                        // onClick={closeGuestsInput}
-                    >
-                    Alterar local/data
-                    <Settings2 className="size-5" />
-                    </button>
-                    
-                </div>
-
-            </div>
-
-            <main className='flex gap-16'>
-                <div className='flex-1'>
-                    <div className='flex items-center justify-between'>
-                        <h2 className='text-3xl font-semibold'>Atividades</h2>
-                        <button
-                        className="flex items-center gap-2 px-5 py-2 font-medium rounded-lg text-lime-950 bg-lime-300 hover:bg-lime-400"
-                        >
-                        <Plus className="size-5" />
-                        Cadastrar atividade
-                        </button>
-                    </div>
-                </div>
-                <div className='max-w-80'></div>
-            </main>
+  return (
+    <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
+      <div className="flex items-center justify-between h-16 px-4 rounded-xl bg-zinc-900 shadow-shape">
+        <div className="flex items-center gap-2">
+          <MapPin className="size-5 text-zinc-400" />
+          <span className="text-zinc-100">Florianópolis, Brasil</span>
         </div>
-    )
-}
- 
-export default TripDetailsPage;
 
+        <div className="flex items-center gap-5">
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400" />
+            <span className="text-zinc-100">17 a 23 de Agosto</span>
+          </div>
+
+          <div className="w-px h-6 bg-zinc-800" />
+
+          <button
+            className="flex items-center gap-2 px-5 py-2 font-medium rounded-lg text-zinc-200 bg-zinc-800 hover:bg-zinc-700"
+            // onClick={closeGuestsInput}
+          >
+            Alterar local/data
+            <Settings2 className="size-5" />
+          </button>
+        </div>
+      </div>
+
+      <main className="flex gap-16">
+        <div className="flex-1 space-y-6">
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-semibold">Atividades</h2>
+            <button className="flex items-center gap-2 px-5 py-2 font-medium rounded-lg text-lime-950 bg-lime-300 hover:bg-lime-400">
+              <Plus className="size-5" />
+              Cadastrar atividade
+            </button>
+          </div>
+
+          <div className="space-y-8">
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">
+                  Dia 17
+                </span>
+                <span className="text-xs text-zinc-500">Sábado</span>
+              </div>
+              <p className="text-zinc-500 text-sm">
+                Nenhuma atividade cadastrada nessa data.
+              </p>
+            </div>
+          </div>
+
+          <div className="space-y-2.5">
+            <div className="flex gap-2 items-baseline">
+              <span className="text-xl text-zinc-300 font-semibold">
+                Dia 17
+              </span>
+              <span className="text-xs text-zinc-500">Sábado</span>
+            </div>
+            <div className="space-y-2">text</div>
+          </div>
+        </div>
+        <div className="max-w-80"> Div ao lado</div>
+      </main>
+    </div>
+  );
+};
+
+export default TripDetailsPage;
