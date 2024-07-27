13ba925f4dd10fbdb468f28edd466a3414837ab2
Valter B Freitas Jr <valter191@hotmail.com>
2024-07-26 22:02:01 -0300

    feat: add activites and importants links section

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -1,4 +1,4 @@
-import { Calendar, MapPin, Plus, Settings2 } from "lucide-react";
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
 
 const TripDetailsPage = () => {
   return (
@@ -17,17 +17,14 @@ const TripDetailsPage = () => {
 
           <div className="w-px h-6 bg-zinc-800" />
 
-          <button
-            className="flex items-center gap-2 px-5 py-2 font-medium rounded-lg text-zinc-200 bg-zinc-800 hover:bg-zinc-700"
-            // onClick={closeGuestsInput}
-          >
+          <button className="flex items-center gap-2 px-5 py-2 font-medium rounded-lg text-zinc-200 bg-zinc-800 hover:bg-zinc-700">
             Alterar local/data
             <Settings2 className="size-5" />
           </button>
         </div>
       </div>
 
-      <main className="flex gap-16">
+      <main className="flex gap-16 px-4">
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Atividades</h2>
@@ -49,19 +46,44 @@ const TripDetailsPage = () => {
                 Nenhuma atividade cadastrada nessa data.
               </p>
             </div>
-          </div>
 
-          <div className="space-y-2.5">
-            <div className="flex gap-2 items-baseline">
-              <span className="text-xl text-zinc-300 font-semibold">
-                Dia 17
-              </span>
-              <span className="text-xs text-zinc-500">Sábado</span>
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">
+                  Dia 18
+                </span>
+                <span className="text-xs text-zinc-500">Domingo</span>
+              </div>
+              <div className="space-y-2.5">
+                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                  <CircleCheck className="size-5 text-lime-300" />
+                  <span className="text-zinc-100">Academia em grupo</span>
+                  <span className="text-zinc-400 text-sm ml-auto">8:00h</span>
+                </div>
+              </div>
+              <div className="space-y-2.5">
+                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                  <CircleCheck className="size-5 text-lime-300" />
+                  <span className="text-zinc-100">Academia em grupo</span>
+                  <span className="text-zinc-400 text-sm ml-auto">8:00h</span>
+                </div>
+              </div>
             </div>
-            <div className="space-y-2">text</div>
           </div>
         </div>
-        <div className="max-w-80"> Div ao lado</div>
+
+        <div className="max-w-80 space-y-6">
+          <div className="space-y-6">
+            <h2 className="font-semibold text-xl">Links importantes</h2>
+            <button className="flex items-center w-full gap-2 px-5 justify-center py-2 font-medium rounded-lg text-zinc-200 bg-zinc-800 hover:bg-zinc-700">
+              <Plus className="size-5" /> Cadastrar novo link
+            </button>
+          </div>
+
+          <div className="w-full h-px bg-zinc-800" />
+
+          <div>text2</div>
+        </div>
       </main>
     </div>
   );
